ac0ad437f4a0784e04752f5998e95a46b3334d0e
alx.grents <aleksandr.grents@z-media.info>
2022-10-04 21:32:29 +0500

    GARAGE-1 Добавил стараницу игры

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInPage from './pages/SignIn';
+import GamePage from "./pages/GamePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignInPage />} />
+        <Route path="/game" element={<GamePage w = {100}/>} />
       </Routes>
     </Router>
   )
