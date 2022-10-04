798bf80b9c12657d9ce8a98674d8f12388dc3e14
matroskin92 <dev@matro.site>
2022-10-04 10:38:24 +0400

    Прикрутил роутер и пример страницы

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,13 @@
-import { useEffect } from 'react'
-import Button from '@mui/material/Button'
-import './App.css'
-import {GamePage} from "./pages/GamePage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignInPage from './pages/SignIn';
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
   return (
-      <div className="App">
-        <Button variant="contained">Hello World</Button>
-        <GamePage w={100}/>
-      </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+      </Routes>
+    </Router>
   )
 }
 
