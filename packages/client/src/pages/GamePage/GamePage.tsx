71fd84293bd5de1518b7cf00c2ee314ec7909e27
alx.grents <aleksandr.grents@z-media.info>
2022-10-05 23:28:34 +0500

    1-GARAGE Добавил страницу игры (без канваса)

---
+++ packages/client/src/pages/GamePage/GamePage.tsx
@@ -0,0 +1,35 @@
+import {CanvasComponent} from "@/components/CanvasComponent";
+import {FC} from "react";
+import healthEmpty from 'public/game/health-empty.png';
+import healthFill from 'public/game/health-fill.png';
+
+import './GamePage.less';
+
+type GamePageProps = {
+    health: number,
+}
+
+export const GamePage: FC<GamePageProps> = ({
+    health,
+}) => {
+    return (
+        <div id="game-page">
+            <div className="header">
+                <div className="healths">
+                    <img src={health >= 1 ? healthFill : healthEmpty} alt="<3"/>
+                    <img src={health >= 2 ? healthFill : healthEmpty} alt="<3"/>
+                    <img src={health >= 3 ? healthFill : healthEmpty} alt="<3"/>
+                </div>
+                <div className="timer">
+                    <span className="value">00:10</span>
+                </div>
+                <div className="score">
+                    <span className="value">200</span>
+                </div>
+            </div>
+            <div className="content">
+                <CanvasComponent id="game-canvas" width={500} height={500}/>
+            </div>
+        </div>
+    )
+}
