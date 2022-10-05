4b510f5192663c46b86a52915eea3734259771f9
matroskin92 <dev@matro.site>
2022-10-05 12:25:14 +0400

    Добавлена тема и тестовая страница

---
+++ packages/client/src/theme.ts
@@ -0,0 +1,26 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+export default responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: 'rgba(21, 255, 241, 1)',
+      },
+      secondary: {
+        main: '#ffffff',
+      },
+      text: {
+        primary: '#ffffff',
+        secondary: '#ffffff',
+        disabled: 'rgba(255,255,255,0.38)',
+      },
+      background: {
+        default: '#141E30',
+        paper: '#141E30',
+      },
+    },
+    shape: {
+      borderRadius: 8,
+    },
+  })
+);
