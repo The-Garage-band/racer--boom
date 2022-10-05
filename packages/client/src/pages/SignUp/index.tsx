faa815ab6ebf00d6593f1f5494174500a6e25a74
matroskin92 <dev@matro.site>
2022-10-05 20:51:56 +0400

    Копирование авторизация/регистрация

---
+++ packages/client/src/pages/SignUp/index.tsx
@@ -0,0 +1,58 @@
+import { Box, OutlinedInput, InputLabel, InputAdornment, FormControl, Link, Button} from '@mui/material';
+import { EmailOutlined, LockOutlined } from '@mui/icons-material';
+import PageLayout from '../../hocs/page-layout';
+
+const SignInPage = () => {
+  return (
+    <PageLayout>
+      <Box className="form">
+        <h1 className="form__title">Вход</h1>
+        <FormControl variant="outlined" fullWidth focused sx={{
+          marginBottom: '1rem'
+        }}>
+          <InputLabel htmlFor="input-login">
+            Логин
+          </InputLabel>
+          <OutlinedInput
+            id="input-login"
+            type="text"
+            startAdornment={
+              <InputAdornment position="start">
+                <EmailOutlined color="secondary" />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl variant="outlined" fullWidth focused sx={{
+          marginBottom: '1rem'
+        }}>
+          <InputLabel htmlFor="input-login">
+            Пароль
+          </InputLabel>
+          <OutlinedInput
+            id="input-login"
+            type="password"
+            startAdornment={
+              <InputAdornment position="start">
+                <LockOutlined color="secondary" />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          width: '100%',
+          marginBottom: '1rem'
+        }}>
+          <Link href="#" color="secondary">Забыли пароль?</Link>
+        </Box>
+        <Button variant="contained" fullWidth sx={{marginBottom: '1rem'}}>Авторизация</Button>
+        <Button variant="text" fullWidth sx={{marginBottom: '1rem'}}>Нет аккаунта?</Button>
+        <Button variant="outlined" fullWidth sx={{marginBottom: '1rem'}}>Регистрация</Button>
+      </Box>
+    </PageLayout>
+  );
+};
+
+export default SignInPage;
