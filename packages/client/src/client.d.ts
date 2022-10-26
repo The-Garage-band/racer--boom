14a436233bfae9552bf10b2319725091dfdb71b7
matroskin92 <dev@matro.site>
2022-10-26 22:05:37 +0400

    Добавлил картинки в type ts

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -2,3 +2,8 @@ declare const __SERVER_PORT__: number
 declare type StringObject = {
   [key: string]: string | undefined
 }
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
