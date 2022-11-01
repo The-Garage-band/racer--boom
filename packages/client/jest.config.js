646d0f4e79eb2fa608811bafc91c22ad9e571e8a
sds90 <smorodinov.denis@gmail.com>
2022-11-01 20:45:03 +0300

    Добавил формат звуков

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -7,7 +7,7 @@ export default {
   //testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
   modulePaths: ["src"],
   moduleNameMapper: {
-    '\\.(css|less|png|jpg)$': 'identity-obj-proxy',
+    '\\.(css|less|png|jpg|mp3)$': 'identity-obj-proxy',
 
     "^@\/(.*)$": "<rootDir>/src/$1",
     "^public\/(.*)$": "<rootDir>/public/$1",
