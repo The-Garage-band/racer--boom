81e82335ee3c09de29b68a18322a88fc77f4ad1e
Alena Bagaeva <xaru123@yandex.ru>
2022-12-18 00:30:36 +0500

    try with param

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -2,16 +2,16 @@ import type {SequelizeOptions} from "sequelize-typescript";
 import {Sequelize} from "sequelize-typescript";
 import {ForumMessage, ForumTheme} from "./models";
 
-// const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
-//   process.env
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
+  process.env
 
 export const createClientAndConnect = async () => {
   const sequelizeOptions: SequelizeOptions = {
     host: '158.160.15.125',
-    port: 5432,
-    username: 'postgres',
-    password: 'root',
-    database: 'postgres',
+    port: Number(POSTGRES_PORT),
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DB,
     dialect: 'postgres',
   };
 
