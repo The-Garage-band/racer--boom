bda2e4af00906c293c93da69d89c3bcfd5b74122
Alena Bagaeva <xaru123@yandex.ru>
2022-12-18 00:56:58 +0500

    f

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -7,6 +7,8 @@ const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
   process.env.NODE_ENV
 
 export const createClientAndConnect = async () => {
+  console.log(POSTGRES_USER,POSTGRES_PASSWORD,POSTGRES_DB,POSTGRES_PORT);
+  console.log(process.env)
   const sequelizeOptions: SequelizeOptions = {
     host: '158.160.15.125',
     port: Number(POSTGRES_PORT),
