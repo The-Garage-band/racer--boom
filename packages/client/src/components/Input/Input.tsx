804112266a2a1c84cf439b4f49dcea855b92b273
matroskin92 <dev@matro.site>
2022-12-19 17:43:04 +0400

    Добавил тесты для Input

---
+++ packages/client/src/components/Input/Input.tsx
@@ -0,0 +1,58 @@
+import {
+  OutlinedInput,
+  InputLabel,
+  InputAdornment,
+  FormControl,
+  FormHelperText,
+} from '@mui/material'
+import {ChangeEvent} from "react";
+import {SvgIconComponent} from "@mui/icons-material";
+
+import { useTheme } from '@mui/material/styles';
+
+export interface InputProps {
+  label: string
+  type: 'text' | 'password' | 'email' | 'tel' | 'file'
+  name: string
+  Icon: SvgIconComponent,
+  sx?: StringObject
+  value?: string
+  error?: boolean
+  helperText?: string
+  onChange?: (event: ChangeEvent) => void,
+}
+
+export default function Input({
+  label,
+  type,
+  name,
+  value,
+  error,
+  helperText,
+  onChange,
+  Icon,
+  sx,
+}: InputProps) {
+
+  const theme = useTheme();
+
+  return (
+    <FormControl variant="outlined" fullWidth sx={sx}>
+      <InputLabel htmlFor="input-login" style={{backgroundColor: theme.palette.background.default}}>{label}</InputLabel>
+      <OutlinedInput
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        error={error}
+        startAdornment={
+          <InputAdornment position="start">
+            <Icon color="secondary" />
+          </InputAdornment>
+        }
+      />
+      <FormHelperText>{helperText}</FormHelperText>
+    </FormControl>
+  )
+}
