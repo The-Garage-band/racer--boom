69698cf5eb316e3db655fc2563283c911a158148
matroskin92 <dev@matro.site>
2022-12-20 14:52:41 +0400

    Простой тест на наличие кнопки

---
+++ packages/client/src/pages/EndGamePage/EndGamePage.test.tsx
@@ -0,0 +1,21 @@
+import { EndGamePage } from "./EndGamePage";
+
+import { render, screen } from '@testing-library/react';
+
+import {BrowserRouter as Router} from 'react-router-dom';
+
+describe('End game page test series', () => {
+
+  test('Should be visible button for return to game', async () => {
+
+    render(
+      <Router>
+        <EndGamePage />
+      </Router>
+    );
+
+    const returnToGameButton = await screen.findByText(/заново/i);
+    expect(returnToGameButton).toBeVisible();
+  });
+
+});
